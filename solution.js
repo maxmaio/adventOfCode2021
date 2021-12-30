d3fa3241f838ef6c34d20591efdb249d9e9c6edd
maxmaio <maxmaio8@gmail.com>
2021-12-30 12:50:46 -0800

    part2 day1

--- solution.js
+++ solution.js
@@ -12,4 +12,22 @@ for (let i = 0; i < input.length; i++) {
     }
     prev = input[i];
 }
-console.log(count);
+console.log("part 1:" + count);
+
+// Part 2
+let a = 0;
+let b = 0;
+let c = 0;
+count = 0;
+for (let i = 0; i < input.length-3; i++) {
+    a = input[i];
+    b = input[i+1];
+    c = input[i+2];
+    d = input[i+3];
+    let current = a + b + c;
+    let next = b + c + d;
+    if (current < next) {
+        count++;
+    }
+}
+console.log("part 2:" + count);
