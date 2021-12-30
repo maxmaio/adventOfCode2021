d4eed08d99678e9d538c517bd66d8594d2af578a
maxmaio <maxmaio8@gmail.com>
2021-12-30 13:18:33 -0800

    day 2: part 2 complete

--- day2/solution.js
+++ day2/solution.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var input = fs.readFileSync('input.txt')
               .toString()
               .split("\n");
-console.log(input);
+
 let x = 0;
 let y = 0;
 let curr;
@@ -19,4 +19,26 @@ for(let i = 0; i < input.length; i++) {
     }
 }
 
-console.log(x * y);
+console.log("part 1: " + (x * y));
+
+// Part 2
+
+
+x = 0;
+y = 0;
+aim = 0;
+
+for(let i = 0; i < input.length; i++) {
+    curr = input[i].split(" ");
+    if (curr[0] === "forward") {
+        x += parseInt(curr[1]);
+        y += (parseInt(curr[1]) * aim);
+    }
+    if(curr[0] === "up") {
+        aim -= parseInt(curr[1]);
+    }
+    if(curr[0] === "down") {
+        aim += parseInt(curr[1]);
+    }
+}
+console.log("part 2: " + (x *y));
