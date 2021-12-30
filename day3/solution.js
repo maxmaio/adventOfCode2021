d1baf58cd7006bdedefb3ae67758fe6d2efd318a
maxmaio <maxmaio8@gmail.com>
2021-12-30 14:03:14 -0800

    day3: part 2 complete

--- day3/solution.js
+++ day3/solution.js
@@ -31,3 +31,53 @@ for (let i = 0; i < count.length; i++) {
 gammaRate = parseInt(gammaRate.join(""), 2);
 epsilonRate = parseInt(epsilonRate.join(""), 2);
 console.log("part 1: " + (gammaRate * epsilonRate));
+
+//part 2
+let part2Input = fs.readFileSync('input.txt')
+              .toString()
+              .split("\n")
+let oxygenList = part2Input;
+let co2List = part2Input;
+
+for (let i = 0; i < input[0].length; i++) {
+    if (oxygenList.length > 1) {
+        let oxygencount = 0;
+        oxygenList.forEach(binaryString =>{
+            if (binaryString.charAt(i) === "0") {
+                oxygencount--;
+            } else {
+                oxygencount++;
+            }
+        });
+
+        oxygenList = oxygenList.filter(binaryString => {
+            if (oxygencount < 0){
+                return binaryString.charAt(i) === "0";
+            }
+            if (oxygencount >= 0) {
+                return binaryString.charAt(i) === "1";
+            }
+        });
+    }
+
+    if (co2List.length > 1) {
+        let co2Count = 0;
+        co2List.forEach(binaryString =>{
+            if (binaryString.charAt(i) === "0") {
+                co2Count--;
+            } else {
+                co2Count++;
+            }
+        });
+
+        co2List = co2List.filter(binaryString => {
+            if (co2Count < 0){
+                return binaryString.charAt(i) === "1";
+            }
+            if (co2Count >= 0) {
+                return binaryString.charAt(i) === "0";
+            }
+        });
+    }
+}
+console.log((parseInt(oxygenList[0],2) * parseInt(co2List[0],2)));
